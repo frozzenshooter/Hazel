1f7caee8cac65f0435538a985ba29bb0cf78796f
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-08-24 18:22:07 +0200

    Premake improved and typo fixed

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,14 +1,17 @@
 #pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
-    
     #ifdef HZ_BUILD_DLL
         #define HAZEL_API __declspec(dllexport)
     #else
         #define HAZEL_API __declspec(dllimport)
     #endif
 #else
-    #error Hazel only supports Windows
+    #error Hazel only supports Windows!
+#endif
+
+#ifdef HZ_DEBUG
+    #define HZ_ENABLE_ASSERTS
 #endif
 
 #ifdef HZ_ENABLE_ASSERTS
--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,13 @@
 workspace "Hazel"
-    architecture "x64"
+	architecture "x64"
+	startproject "Sandbox"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"    
-    }
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
@@ -14,128 +15,124 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
-IncludeDir["ImGui"] = "Hazel/vendor/imgui/"
-
+IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
 include "Hazel/vendor/imgui"
 
-startproject "Sandbox"
-
 project "Hazel"
-    location "Hazel"
-    kind "SharedLib"
-    language "C++"
+	location "Hazel"
+	kind "SharedLib"
+	language "C++"
+	staticruntime "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    pchheader "hzpch.h"
-    pchsource "Hazel/src/hzpch.cpp"
+	pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
 
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "%{prj.name}/src",
+	includedirs
+	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}"
-    }
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}"
+	}
 
-    links 
+	links 
 	{ 
-        "GLFW",
-        "Glad",
-        "ImGui",
+		"GLFW",
+		"Glad",
+		"ImGui",
 		"opengl32.lib"
 	}
 
-    filter "system:windows"
-        cppdialect "C++17"
-        staticruntime "On"
-        systemversion "latest"
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
 
-        defines
-        {
-            "HZ_PLATFORM_WINDOWS",
-            "HZ_BUILD_DLL",
-            "GLFW_INCLUDE_NONE"
-        }
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
 
-        postbuildcommands
-        {
-            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-        }
-    
-    filter "configurations:Debug"
-        defines "HZ_DEBUG"
-        buildoptions "/MDd"
-        symbols "On"
-    
-    filter "configurations:Release"
-        defines "HZ_RELEASE"
-        buildoptions "/MD"
-        optimize "On"
-    
-    filter "configurations:Dist"
-        defines "HZ_DIST"
-        buildoptions "/MD"
-        optimize "On"
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "On"
 
 project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-    includedirs 
-    {
-        "Hazel/vendor/spdlog/include",
-        "Hazel/src"
-    }
+	includedirs
+	{
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
 
-    links
-    {
-        "Hazel"
-    }
+	links
+	{
+		"Hazel"
+	}
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 
-        defines
-        {
-            "HZ_PLATFORM_WINDOWS"
-        }
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
 
-    filter "configurations:Debug"
-        defines "HZ_DEBUG"
-        buildoptions "/MDd"
-        symbols "On"
-    
-    filter "configurations:Release"
-        defines "HZ_RELEASE"
-        buildoptions "/MD"
-        optimize "On"
-    
-    filter "configurations:Dist"
-        defines "HZ_DIST"
-        buildoptions "/MD"
-        optimize "On"
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "On"
